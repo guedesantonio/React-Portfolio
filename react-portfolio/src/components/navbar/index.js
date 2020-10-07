df7b2e3186884fd464fa7ad643a8247077342fda
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-07 20:35:29 +1100

    componentDidMount created

--- react-portfolio/src/components/navbar/index.js
+++ react-portfolio/src/components/navbar/index.js
@@ -1,4 +1,9 @@
 import React from "react";
+import Background from "./background.jsx";
+import About from "./about.jsx";
+import Projects from "./projects.jsx";
+import Contact from "./contact.jsx";
+import WOW from "wowjs";
 
 class Navbar extends React.Component {
 	constructor(props) {
@@ -6,10 +11,12 @@ class Navbar extends React.Component {
 		this.about = React.createRef();
 		this.projects = React.createRef();
 		this.contact = React.createRef();
-
 		this.scrolling = this.scrolling.bind(this);
 	}
 
+    componentDidMount() {
+		new WOW.WOW().init();
+	}
 
 	render() {
 		 }
