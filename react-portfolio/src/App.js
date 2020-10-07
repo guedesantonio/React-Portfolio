68592e1af7948e5c99345c1b1a8a47c301373183
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-07 16:49:49 +1100

    Navbar component done

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
  return (
-  <Header/>
+  <Wrapper>
+    <Navbar/>
+  </Wrapper>
  );
 }
 
