b438e84d7081a367591cea231170760af6ae67a7
Antonio Guedes <gguedesantonio@gmail.com>
2020-10-10 20:54:42 +1100

    Updates

---
+++ precache-manifest.3114e7ff283969c100980178b11e58f7.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96360eb2610ce0dd5c781ebacd3b317f",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "3551840bd91b7f0e9173",
+    "url": "/React-Portfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "bb589e090d4a46f719bd",
+    "url": "/React-Portfolio/static/css/main.29c430bb.chunk.css"
+  },
+  {
+    "revision": "3551840bd91b7f0e9173",
+    "url": "/React-Portfolio/static/js/2.1ebb95ba.chunk.js"
+  },
+  {
+    "revision": "88a113fe4ec5ef613b063e9745ea130a",
+    "url": "/React-Portfolio/static/js/2.1ebb95ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb589e090d4a46f719bd",
+    "url": "/React-Portfolio/static/js/main.dce5480d.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "4f6324f9334030fff591a5e89c4283fc",
+    "url": "/React-Portfolio/static/media/ANTONIO GUEDES CV.4f6324f9.pdf"
+  },
+  {
+    "revision": "9f4cbfe79ba987d6fc81ab257b177a40",
+    "url": "/React-Portfolio/static/media/Employee-Tracker.9f4cbfe7.png"
+  },
+  {
+    "revision": "4e3e6920d8f6764a496e3d4720b26c41",
+    "url": "/React-Portfolio/static/media/Job-Seeker.4e3e6920.png"
+  },
+  {
+    "revision": "cc8a87f413d0de0d2a0cfbac168b60ba",
+    "url": "/React-Portfolio/static/media/Team-Profile-Generator.cc8a87f4.png"
+  },
+  {
+    "revision": "525e9b74aa99cf1336635171300c74da",
+    "url": "/React-Portfolio/static/media/Want-to-Watch-Series-Manager.525e9b74.png"
+  },
+  {
+    "revision": "d83b2afe16dcc448fdf8401bf7a9c93d",
+    "url": "/React-Portfolio/static/media/Weather-dashboard.d83b2afe.png"
+  },
+  {
+    "revision": "9acb786e22a7e4ea73e157917e96543b",
+    "url": "/React-Portfolio/static/media/bg-qualificacoes.9acb786e.jpg"
+  },
+  {
+    "revision": "470430322f7bb74c01040e40d29550de",
+    "url": "/React-Portfolio/static/media/code-quiz.47043032.gif"
+  },
+  {
+    "revision": "302a61acb260fa1c0c1c2ddc6c01d777",
+    "url": "/React-Portfolio/static/media/home2.302a61ac.png"
+  },
+  {
+    "revision": "ac27618e36b417023c2c9b318a664ff0",
+    "url": "/React-Portfolio/static/media/map-image2.ac27618e.png"
+  },
+  {
+    "revision": "8ed04cf3ab94293925546eb2412f97a8",
+    "url": "/React-Portfolio/static/media/note-taker.8ed04cf3.gif"
+  },
+  {
+    "revision": "0d55b663b9ea28fb7bdf5b414ec7df0b",
+    "url": "/React-Portfolio/static/media/password-generator.0d55b663.png"
+  },
+  {
+    "revision": "9f38217ab187dddce5462149e5dcb991",
+    "url": "/React-Portfolio/static/media/udemy-2-logo.9f38217a.png"
+  },
+  {
+    "revision": "e0c168d722c04d71fcc910bcc8c43a80",
+    "url": "/React-Portfolio/static/media/ufmg-logo-225.e0c168d7.png"
+  },
+  {
+    "revision": "88055e12754b57cfb8d47d56ee09aad8",
+    "url": "/React-Portfolio/static/media/universityofsydneyLogo.88055e12.jpg"
+  },
+  {
+    "revision": "1a3a635ebceee80b0179a233cac99d7c",
+    "url": "/React-Portfolio/static/media/work-day-scheduler.1a3a635e.gif"
+  }
+]);
